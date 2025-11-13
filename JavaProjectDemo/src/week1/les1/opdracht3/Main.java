661ab1652a943f56859c8bb0312d672f93c67931
Jesse <Jessedekoe03@gmail.com>
2025-11-13 21:44:53 +0100

    Werkboek opdracht 1_5

--- JavaProjectDemo/src/week1/les1/opdracht3/Main.java
+++ JavaProjectDemo/src/week1/les1/opdracht3/Main.java
@@ -3,9 +3,15 @@ package week1.les1.opdracht3;
 public class Main {
     public static void main(String[] args) {
         Student student1 = new Student("Jesse de Koe");
-        Student student2 = new Student("Mark Knopfler");
+        System.out.println("naam: " + student1.getNaam());
+        System.out.println("nummer: " + student1.getStudentNummer());
+        student1.setStudentNummer(12345678);
+        System.out.println("toString: " + student1.toString());
 
-        System.out.printf("getter: %s \ntoString: %s\n", student1.getNaam(), student1.toString());
-        System.out.printf("getter: %s \ntoString: %s\n", student2.getNaam(), student2.toString());
+        Student student2 = new Student("Mark Knopfler", 1235672);
+        System.out.println("\nnaam: " + student2.getNaam());
+        System.out.println("nummer: " + student2.getStudentNummer());
+        System.out.println("toString: " + student2.toString());
+
     }
 }
--- JavaProjectDemo/src/week1/les1/opdracht3/Student.java
+++ JavaProjectDemo/src/week1/les1/opdracht3/Student.java
@@ -2,9 +2,15 @@ package week1.les1.opdracht3;
 
 public class Student {
     private String naam;
+    private Integer studentNummer = 0;
 
     public Student (String naam) {
         this.naam = naam;
+    }
+
+    public Student (String naam, Integer studentNummer) {
+        this.naam = naam;
+        this.studentNummer = studentNummer;
     }
 
     public String getNaam() {
@@ -13,6 +19,18 @@ public class Student {
 
     @Override
     public String toString() {
-        return "Deze student heet " + naam;
+        String s = "Deze student heet " + naam;
+        if (studentNummer > 0) {
+            s += " en heeft nummer: " + studentNummer;
+        }
+        return s;
+    }
+
+    public Integer getStudentNummer() {
+        return studentNummer;
+    }
+
+    public void setStudentNummer(Integer studentNummer) {
+        this.studentNummer = studentNummer;
     }
 }
