0c1dddb15bc0c5af696f3201d73c6d38e5845770
Jesse <Jessedekoe03@gmail.com>
2025-11-13 20:16:50 +0100

    Werkboek opdracht 1_2

---
+++ JavaProjectDemo/src/week1/les1/opdracht1/Main.java
@@ -0,0 +1,10 @@
+package week1.les1.opdracht1;
+
+public class Main {
+    public static void main(String[] args){
+        /**
+         * Werkboek opdracht 1_1
+         */
+        System.out.println("Jesse de Koe");
+    }
+}
