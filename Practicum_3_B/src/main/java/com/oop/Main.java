349e7a336c4161466b6773d96cff242bfd6e7ff2
Jesse <Jessedekoe03@gmail.com>
2025-11-28 00:47:53 +0100

    Practicum_3_B v1.1 explaining code and checked/ unchecked exceptions

--- Practicum_3_B/src/main/java/com/oop/Main.java
+++ Practicum_3_B/src/main/java/com/oop/Main.java
@@ -21,5 +21,35 @@ public class Main {
 
         System.out.println(c1);
         System.out.println(c2);
+
+        /**
+         * Explaining the output of the following code:
+         * c1 = new Cirkel(10, 0, 0);
+         * c2 = new Cirkel(0, 10, 10);
+         * - see the stracktrace_image.png for the output -
+         *
+         * as you can see a trace has been set for us to following and find where in the code it exactly goes wrong
+         * we can see the exact steps happened at runtime so we can lead it back to a cause.
+         * in this example we know what the cause is and that is because we say in the cirkel constructor:
+         *         if (rad <= 0) {
+         *             throw new IllegalArgumentException("Radius moet groter dan 0 zijn!");
+         *         }
+         * throw a new instance of that exception if the conditions are true.
+         * now since the IllegalArgumentException is an unchecked exception it is okay if it lives inside your application,
+         * with that I mean that it could already exist in the code and just happen at runtime if conditions are not met.
+         * if we do not catch those exceptions it would crash our application... :-(
+         *
+         * with the following code we create a checked exception:
+         * FileReader file = new FileReader("not_a_file.txt");
+         * - see the not_a_file_error_line_ide.png image for fuller context and where the IDE helps us with -
+         *
+         * as you can see on the image is that the IDE helps us already with the checked exceptions,
+         * by pointing it out.
+         *
+         * With checked exceptions you CANNOT place it in your code and compile it without
+         * having a compile-time-error.
+         * - see stacktrace_checked_expression_file_reader.png to see the stacktrace -
+         *
+         */
     }
 }
