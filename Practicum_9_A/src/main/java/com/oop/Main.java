ae736337c8c573cf3fd80c605638d02f3ed1ccb7
Jesse <Jessedekoe03@gmail.com>
2026-01-16 22:03:27 +0100

    Practicum_9_A V1.0

---
+++ Practicum_9_A/src/main/java/com/oop/Main.java
@@ -0,0 +1,12 @@
+package com.oop;
+
+public class Main {
+    public static void main(String[] arg) {
+        String test1 = Utils.euroBedrag(3.11314);
+        String test2 = Utils.euroBedrag(3.11314, 1);
+        System.out.println(test1);
+        System.out.println(test2);
+
+        System.out.println(Utils.aangeroepen());
+    }
+}
---
+++ Practicum_9_A/src/main/java/com/oop/Utils.java
@@ -0,0 +1,19 @@
+package com.oop;
+
+abstract class Utils {
+
+    protected static int aangeroepen = 0;
+
+    public static String euroBedrag(double bedrag) {
+        return euroBedrag(bedrag, 2);
+    }
+
+    public static String euroBedrag(double bedrag, int precisie) {
+        aangeroepen++;
+        return String.format("€%." + precisie + "f", bedrag);
+    }
+
+    public static int aangeroepen() {
+        return aangeroepen;
+    }
+}
